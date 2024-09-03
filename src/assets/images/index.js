434f735d5b853bfa310ba7d829c21f54045a2fcf
AbdulRaufHassan <raufh1093@gmail.com>
2024-09-03 19:34:53 +0500

    change image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,9 @@
 import FLAG_ICON from "./flag_icon.png";
-import PROFILE_IMG from "./profile_img.jfif";
-import TELESCOPE_IMG from "./telescope_icon.png"
+import PROFILE_IMG from "./profile_img.png";
+import TELESCOPE_IMG from "./telescope_icon.png";
 
 export const images = {
   FLAG_ICON,
   PROFILE_IMG,
-  TELESCOPE_IMG
+  TELESCOPE_IMG,
 };
