47847faf97d2e1f8a6c19e08e495b9a2d76f4451
AbdulRaufHassan <raufh1093@gmail.com>
2024-09-04 11:00:20 +0500

    fixed patient stats chart width

--- src/components/PatientStats.jsx
+++ src/components/PatientStats.jsx
@@ -5,13 +5,15 @@ import RenderPatientStatsChart from "./RenderPatientStatsChart";
 
 function PatientStats({ infoType, value, progressColorObj }) {
   return (
-    <div className="bg-white flex flex-col chart_boxShdw w-full sm:w-[400px] h-[200px] rounded-xl p-3">
+    <div className="bg-white flex flex-col chart_boxShdw w-full lg:w-[400px] h-[200px] md:h-[240px] lg:h-[200px] rounded-xl p-3">
       <div className="p-3 flex flex-col flex-1">
-        <h6 className="hdngColor font-semibold tracking-wide text-xl">
+        <h6 className="hdngColor font-semibold tracking-wide text-xl md:text-2xl lg:text-xl">
           {infoType}
         </h6>
         <div className="flex items-center flex-1 justify-between">
-          <h5 className="text-[#28A745] font-semibold text-[33px]">{value}</h5>
+          <h5 className="text-[#28A745] font-semibold text-[33px] md:text-[40px] lg:text-[33px]">
+            {value}
+          </h5>
           <RenderPatientStatsChart infoType={infoType} />
         </div>
       </div>
